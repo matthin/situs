e97e2cd8b3f69b577f52314879c1ee079c0101b2
Justin Harrison <justin@matthin.com>
2016-05-01 18:57:54 -0400

    Add documentation

--- include/situs/array.h
+++ include/situs/array.h
@@ -2,6 +2,10 @@
 
 #include <sys/types.h>
 
+/**
+ * Dynamic array, which grows and shrinks at runtime. Shouldn't contain
+ * pointers to read-only memory, such as string literals.
+ */
 typedef struct STArray {
   void **elements;
   const size_t element_size;
@@ -9,11 +13,33 @@ typedef struct STArray {
   size_t reserved_length;
 } STArray;
 
+/**
+ * Initializes an array's members.
+ */
 void st_array_new(STArray *array);
+
+/**
+ * Cleanups an array's members. Doesn't free the array itself.
+ */
 void st_array_free(STArray *array);
 
+/**
+ * Adds a single element to the end of an array.
+ */
 void st_array_push(STArray *array, void *element);
+
+/**
+ * Retrieves a single element from an array.
+ */
 void *st_array_get(const STArray *array, size_t index);
+
+/**
+ * Removes a single element from an array.
+ */
 void st_array_remove(STArray *array, size_t index);
 
+/**
+ * Resizes an array's reserved length, which is the space allocated for each
+ * element.
+ */
 void st_array_resize(STArray *array);
--- include/situs/stack.h
+++ include/situs/stack.h
@@ -1,15 +1,31 @@
 #pragma once
 
+/**
+ * Used to store an element in a stack.
+ */
 typedef struct STNode {
   void *data;
   struct STNode *next;
 } STNode;
 
+/**
+ * Standard stack which accesses data using the last in, first out method.
+ */
 typedef struct STStack {
   STNode *top;
 } STStack;
 
+/**
+ * Cleanups a stack's members. Doesn't free the stack itself.
+ */
 void st_stack_free(STStack *stack);
 
+/**
+ * Adds a single element onto a stack.
+ */
 void st_stack_push(STStack *stack, void *element);
+
+/**
+ * Removes and returns a single element from a stack.
+ */
 void *st_stack_pop(STStack *stack);
